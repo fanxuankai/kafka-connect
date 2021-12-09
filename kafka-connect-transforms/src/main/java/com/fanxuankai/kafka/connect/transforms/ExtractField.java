3323aebc5597d63525cefd99534eb98f0e996088
fanxuankai <fan_xuankai@126.com>
2021-12-09 13:39:23 +0800

    ExtractField 优化

--- kafka-connect-transforms/src/main/java/com/fanxuankai/kafka/connect/transforms/ExtractField.java
+++ kafka-connect-transforms/src/main/java/com/fanxuankai/kafka/connect/transforms/ExtractField.java
@@ -1,6 +1,7 @@
 package com.fanxuankai.kafka.connect.transforms;
 
 import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
@@ -9,6 +10,7 @@ import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.util.SimpleConfig;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.apache.kafka.connect.transforms.util.Requirements.requireMapOrNull;
@@ -18,43 +20,52 @@ import static org.apache.kafka.connect.transforms.util.Requirements.requireStruc
  * @author fanxuankai
  */
 public abstract class ExtractField<R extends ConnectRecord<R>> implements Transformation<R> {
-
     public static final String OVERVIEW_DOC =
             "Extract the specified fields from a Struct when schema present, or a Map in the case of schemaless data. "
                     + "Any null values are passed through unmodified."
                     + "<p/>Use the concrete transformation type designed for the record key (<code>" + Key.class.getName() + "</code>) "
                     + "or value (<code>" + Value.class.getName() + "</code>).";
-
     private static final String FIELDS_CONFIG = "fields";
-
     public static final ConfigDef CONFIG_DEF = new ConfigDef()
-            .define(FIELDS_CONFIG, ConfigDef.Type.STRING, ConfigDef.NO_DEFAULT_VALUE, ConfigDef.Importance.MEDIUM,
-                    "Fields name to extract.");
+            .define(FIELDS_CONFIG, ConfigDef.Type.LIST, ConfigDef.NO_DEFAULT_VALUE, new ConfigDef.Validator() {
+                        @SuppressWarnings("unchecked")
+                        @Override
+                        public void ensureValid(String name, Object valueObject) {
+                            List<String> value = (List<String>) valueObject;
+                            if (value == null || value.isEmpty()) {
+                                throw new ConfigException("Must specify at least one field to extract.");
+                            }
+                            parseFieldTypes(value);
+                        }
+
+                        @Override
+                        public String toString() {
+                            return "list of pairs, e.g. <code>foo:bar,abc:xyz</code>";
+                        }
+                    },
+                    ConfigDef.Importance.HIGH, "Fields name to extract.");
 
     private static final String PURPOSE = "fields extraction";
-
+    private static final String WHOLE_VALUE_FIELD = null;
     private Map<String, String> fieldNameByTopic;
-    private static final String REGEX = ";";
+    private String wholeValueExtractField;
+    private static final String REGEX = ":";
 
     @Override
     public void configure(Map<String, ?> props) {
         final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
-        String fields = config.getString(FIELDS_CONFIG);
-        String[] fieldNames = fields.split(REGEX);
-        int odd = fieldNames.length & 1;
-        if (odd == 1) {
-            // 不能为奇数
-            throw new IllegalArgumentException("Unknown fields: " + fields);
-        }
-        fieldNameByTopic = new HashMap<>(16);
-        for (int i = 0; i < fieldNames.length; i++) {
-            fieldNameByTopic.put(fieldNames[i], fieldNames[++i]);
-        }
+        fieldNameByTopic = parseFieldTypes(config.getList(FIELDS_CONFIG));
+        wholeValueExtractField = fieldNameByTopic.get(WHOLE_VALUE_FIELD);
     }
 
     @Override
     public R apply(R record) {
-        String fieldName = fieldNameByTopic.get(record.topic());
+        String fieldName;
+        if (wholeValueExtractField != null) {
+            fieldName = wholeValueExtractField;
+        } else {
+            fieldName = fieldNameByTopic.get(record.topic());
+        }
         final Schema schema = operatingSchema(record);
         if (schema == null) {
             final Map<String, Object> value = requireMapOrNull(operatingValue(record), PURPOSE);
@@ -120,6 +131,28 @@ public abstract class ExtractField<R extends ConnectRecord<R>> implements Transf
             return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(),
                     updatedSchema, updatedValue, record.timestamp());
         }
+    }
+
+    private static Map<String, String> parseFieldTypes(List<String> keys) {
+        final Map<String, String> m = new HashMap<>();
+        boolean isWholeValueCast = false;
+        for (String key : keys) {
+            final String[] parts = key.split(REGEX);
+            if (parts.length > 2) {
+                throw new ConfigException(ExtractField.FIELDS_CONFIG, keys, "Invalid keys: " + key);
+            }
+            if (parts.length == 1) {
+                m.put(WHOLE_VALUE_FIELD, parts[0].trim());
+                isWholeValueCast = true;
+            } else {
+                m.put(parts[0].trim(), parts[1].trim());
+            }
+        }
+        if (isWholeValueCast && keys.size() > 1) {
+            throw new ConfigException("Cast transformations that specify a type to cast the entire value to "
+                    + "may ony specify a single cast in their spec");
+        }
+        return m;
     }
 
 }
